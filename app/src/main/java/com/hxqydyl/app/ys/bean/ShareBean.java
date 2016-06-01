bb03079dac32618a311c48716e2856fb075e79ed
wangxu <wangxu>
2016-06-01 14:12:23 +0800

    补充提交资源

---
+++ app/src/main/java/com/hxqydyl/app/ys/bean/ShareBean.java
@@ -0,0 +1,43 @@
+package com.hxqydyl.app.ys.bean;
+
+/**
+ * Created by wangxu on 2016/6/1.
+ */
+public class ShareBean {
+    private String title;
+    private String desc;
+    private String img;
+    private String link;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
